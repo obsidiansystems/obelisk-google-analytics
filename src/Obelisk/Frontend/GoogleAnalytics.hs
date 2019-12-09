bd40d8aa1d46fd673f2a2549572e00927ef0b33a
leon.smith@obsidian.systems <leon.smith@obsidian.systems>
2019-12-09 16:45:00 -0500

    WIP adjust type of runGoogleAnalyticsT

--- src/Obelisk/Frontend/GoogleAnalytics.hs
+++ src/Obelisk/Frontend/GoogleAnalytics.hs
@@ -75,18 +75,14 @@ runGoogleAnalyticsT
      , PerformEvent t m
      , Routed t r m
      )
-  => Maybe Text  -- ^ tracking id,  see <https://analytics.google.com/analytics/web/>.  If 'Nothing', then disable Google Analytics.
-  -> (w -> GtagJSCall)
-  -> GoogleAnalyticsT t w m ()
-  -> m ()
+  => (w -> GtagJSCall)
+  -> GoogleAnalyticsT t w m a
+  -> m a
 
-runGoogleAnalyticsT mTrackingId interpW (GoogleAnalyticsT m) = do
-  case mTrackingId of
-    Nothing -> return ()
-    Just trackingId -> do
-      googleAnalytics trackingId
-      (_, evt) <- runEventWriterT m
-      performEvent_ ( gtag <$> evt )
+runGoogleAnalyticsT interpW (GoogleAnalyticsT m) = do
+  (a, evt) <- runEventWriterT m
+  performEvent_ ( gtag <$> evt )
+  return a
   where
     gtag ws = do
       forM_ ws $ \w -> do
@@ -100,38 +96,37 @@ runGoogleAnalyticsT mTrackingId interpW (GoogleAnalyticsT m) = do
 instance (Reflex t, Monad m) => Analytics t w (GoogleAnalyticsT t w m) where
   tellAnalytics =  GoogleAnalyticsT . tellEvent . (Seq.singleton <$>)
 
+type TrackingId = Text
+
 -- | Config path for the tracking ID
 trackingIdPath :: Text
 trackingIdPath = "frontend/google/analytics/tracking-id"
 
-type TrackingId = Text
-
 -- | Embed Google analytics scripts. This function will throw if the tracking ID
 -- config is missing.
 googleAnalyticsFromConfig :: (HasConfigs m, Prerender js t m, PerformEvent t m, DomBuilder t m, Routed t r m) => m ()
-googleAnalyticsFromConfig = getTrackingId >>= \case
-   Nothing -> fail $ T.unpack $ T.unwords
-     [ "Google analytics tracking ID not found in configs."
-     , "Expected path: " <> trackingIdPath
-     ]
-   Just tid -> googleAnalytics tid
+googleAnalyticsFromConfig = getTrackingId >>= googleAnalytics
 
 -- | Embed Google analytics scripts with the given tracking ID.
-googleAnalytics :: (DomBuilder t m, Prerender js t m, PerformEvent t m, Routed t r m) => TrackingId -> m ()
-googleAnalytics trackingId = do
-  let gtagSrc = "https://www.googletagmanager.com/gtag/js?id=" <> trackingId
-      escapedTrackingId = T.pack $ show trackingId   -- TODO: is this realy the correct way to escape?
-  elAttr "script" ("async" =: "async" <> "src" =: gtagSrc) blank
-  el "script" $ text $ T.unlines
-    [ "window.dataLayer = window.dataLayer || [];"
-    , "function gtag(){dataLayer.push(arguments);}"
-    , "gtag('js', new Date());"
-    , "gtag('config', " <> escapedTrackingId <> ");"
-    ]
-  route <- askRoute
-  prerender_ blank $ do
-    performEvent_ $ ffor (updated route) $ \_ -> liftJSM $ do
-      void $ eval $ "gtag('config', " <> escapedTrackingId <> ", { 'page_path': location.pathname, 'page_title': document.title });"
+googleAnalytics :: (DomBuilder t m, Prerender js t m, PerformEvent t m, Routed t r m) => Maybe TrackingId -> m ()
+googleAnalytics mTrackingId = do
+  case mTrackingId of
+    Nothing -> do
+      el "script" $ text "function gtag(){return;}"
+    Just trackingId -> do
+      let gtagSrc = "https://www.googletagmanager.com/gtag/js?id=" <> trackingId
+          escapedTrackingId = T.pack $ show trackingId   -- TODO: is this realy the correct way to escape?
+      elAttr "script" ("async" =: "async" <> "src" =: gtagSrc) blank
+      el "script" $ text $ T.unlines
+        [ "window.dataLayer = window.dataLayer || [];"
+        , "function gtag(){dataLayer.push(arguments);}"
+        , "gtag('js', new Date());"
+        , "gtag('config', " <> escapedTrackingId <> ");"
+        ]
+      route <- askRoute
+      prerender_ blank $ do
+        performEvent_ $ ffor (updated route) $ \_ -> liftJSM $ do
+          void $ eval $ "gtag('config', " <> escapedTrackingId <> ", { 'page_path': location.pathname, 'page_title': document.title });"
 
 -- | Get the tracking ID from the configuration. The config file should be
 -- located at 'trackingIdPath'.
